9a9624993e0def03a36fc1c220097ab52b642cb4
Martino Sebastiani <49372476+BaseMar@users.noreply.github.com>
2026-04-25 12:56:59 +0200

    Create weather_observation_1.sql with query
    Add SQL query to select city and state from station.

---
+++ sql/weather_observation_1.sql
@@ -0,0 +1,11 @@
+-- ========================
+--        Info
+-- ========================
+-- Weather Observation Station 1
+-- Direct link: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+-- ========================
+--       Solution
+-- ========================
+SELECT city, state
+FROM station;
